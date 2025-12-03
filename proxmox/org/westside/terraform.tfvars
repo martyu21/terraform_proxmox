4cebae8292a9828c6bdd7b6f27faafa55e287d6c
Martyn Little <martyn.little@outlook.com>
2025-12-03 21:31:35 +0000

    Update to variables

--- proxmox/org/westside/terraform.tfvars
+++ proxmox/org/westside/terraform.tfvars
@@ -2,9 +2,3 @@ vms = [
   { name = "M02", cores = 2, memory = 2048 },
   { name = "M03", cores = 2, memory = 2048 }
 ]
-
-proxmox_node        = "westside"
-template_name       = "testvm01"
-vm_disk_size        = "40G"
-vm_storage          = "local-lvm"
-vm_bridge           = "vmbr0"
--- proxmox/variables.tf
+++ proxmox/variables.tf
@@ -1,10 +1,22 @@
-variable "proxmox_node" { default = "pve" }
-variable "template_name" {}
+
+variable "proxmox_node" {
+  type        = string
+  description = "Defines the proxmox node"
+  default     = "westside"
+}
+
+variable "template_name" {
+  type        = string
+  description = "Defines the template to clone from"
+  default     = "testvm01"
+}
+
 variable "vm_cores" { default = 2 }
 variable "vm_memory" { default = 4096 }
 variable "vm_disk_size" { default = "40G" }
 variable "vm_storage" { default = "local-lvm" }
 variable "vm_bridge" { default = "vmbr0" }
+
 variable "vms" {
   description = "VM definitions"
   type = list(object({
